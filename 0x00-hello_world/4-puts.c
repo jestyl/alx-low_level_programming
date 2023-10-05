9cd4a96d72a021c24a42d8dcca0d98a93f5926b8
Jestyl <jestylarku@gmail.com>
2023-10-05 14:26:00 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - Puts function
+ * main - Puts functions must work
  * Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle
-");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
