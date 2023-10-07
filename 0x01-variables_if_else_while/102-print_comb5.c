ab44699dd03c9c47067dc7fd8a427b329be99fb7
Jestyl <jestylarku@gmail.com>
2023-10-06 19:11:23 -0700

    Print comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,36 +8,29 @@
 
 int main(void)
 {
-	int tho;
-	int hun;
-	int ten;
-	int bas;
+    int n = 0;
 
-for ( tho = 0 ; tho < 10 ; tho++)
-{	
+    while (n < 10000)
+    {
+        if (n / 100 < n % 100)
+        {
+            putchar((n / 1000) + '0');
+            putchar(((n / 100) % 10) + '0');
+            putchar(' ');
+            putchar(((n / 10) % 10) + '0');
+            putchar((n % 10) + '0');
 
-	for (hun = 0 ; hun <= 10 ; hun++)
-	{
-	
-		for (ten = 0 ; ten < 10 ; ten++)
-		{
-			for ( bas = 0 ; bas < 10 ; bas++)
-			{
-				putchar('0' + tho);
-				putchar('0' + hun);
-				putchar(32);
-				putchar('0' + ten);
-				putchar('0' + bas);
-				if (!(tho == 9 && hun == 8))
-				{
-					putchar(',');
-					putchar(32);
-				}
-				bas++;
-			}
-		}
-	}
-}
-	putchar('\n');
-	return (0);
+            if (n != 9899)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+
+        n++;
+    }
+
+    putchar('\n');
+
+    return (0);
 }
