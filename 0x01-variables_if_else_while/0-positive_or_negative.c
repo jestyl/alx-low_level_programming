2d674e7c0f31b9ec570b2ed6c021e7ce8e84b959
Jestyl <jestylarku@gmail.com>
2023-10-06 18:20:01 -0700

    Document done

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - declare state of number
- * return = 0
+ * main - Determines if a number is positive, negative or zero.
+ * Return: 0 (Success):
  */
 
 int main(void)
