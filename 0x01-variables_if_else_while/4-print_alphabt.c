37b57c755144410d1dee003d7bd8d294a43232f7
Jestyl <jestylarku@gmail.com>
2023-10-06 18:57:37 -0700

    Persistence

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+  * Return: 0 (Sucess)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		if ((c == 'q' || c == 'e') != 1)
+		{
+			putchar(c);
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
