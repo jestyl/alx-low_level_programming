3cd53ab2cdd12077936eb090e09105d632c956e5
Jestyl <jestylarku@gmail.com>
2024-01-15 22:20:21 -0800

    dynamic Library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -c -Wall -Werror -Wextra -pedantic -fpic *.c
+gcc -Wall -Werror -Wextra -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
